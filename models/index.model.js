cdfbc04ed255e70377e918030612a6c70e7aac7b
PhanQuang <phanquang1830@gmail.com>
2025-06-02 22:08:06 +0700

    Feat: add index model

---
+++ models/index.model.js
@@ -0,0 +1,49 @@
+import sequelize from "../config/db.js";
+
+import AccountModel from "./account.model.js";
+import AccountGIDetailModel from "./accountGIDetail.model.js";
+import AccountLQDetailModel from "./accountLQDetail.model.js";
+import AccountWWDetailModel from "./accountWWDetail.model.js";
+
+//Khởi tạo model
+const Account = AccountModel(sequelize);
+const AccountGIDetail = AccountGIDetailModel(sequelize);
+const AccountLQDetail = AccountLQDetailModel(sequelize);
+const AccountWWDetail = AccountWWDetailModel(sequelize);
+
+// Thiết lập quan hệ
+// Account & AccountGIDetail
+Account.hasOne(AccountGIDetail, {
+    foreignKey: 'account_gi_id',
+    onDelete: 'CASCADE'
+});
+AccountGIDetail.belongsTo(Account, {
+    foreignKey: 'account_gi_id'
+});
+
+// Account & AccountLQDetail
+Account.hasOne(AccountLQDetail, {
+    foreignKey: 'account_lq_id',
+    onDelete: 'CASCADE'
+});
+AccountLQDetail.belongsTo(Account, {
+    foreignKey: 'account_lq_id'
+});
+
+// Account & AccountWWDetail
+Account.hasOne(AccountWWDetail, {
+    foreignKey: 'account_ww_id',
+    onDelete: 'CASCADE'
+});
+AccountWWDetail.belongsTo(Account, {
+    foreignKey: 'account_ww_id'
+});
+
+// Export lại sequelize để khi import thì import chung cho tiện
+export {
+    sequelize,
+    Account,
+    AccountGIDetail,
+    AccountLQDetail,
+    AccountWWDetail
+}
