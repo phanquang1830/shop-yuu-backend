31e3890877eaf6cc5a93dfbd7728910b7c48a7a4
PhanQuang <phanquang1830@gmail.com>
2025-06-02 22:06:50 +0700

    Feat: add account lq detail model

---
+++ models/accountLQDetail.model.js
@@ -0,0 +1,27 @@
+import { DataTypes } from "sequelize";
+
+export default (sequelize) =>{
+    const AccountLQDetail = sequelize.define('AccountLQDetail', {
+        account_lq_id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            allowNull: false 
+        },
+        rank: {
+            type: DataTypes.STRING(50)
+        },
+        lever: {
+            type: DataTypes.INTEGER
+        },
+        hero_count: {
+            type: DataTypes.INTEGER
+        },
+        skin_count: {
+            type: DataTypes.INTEGER
+        }
+    }, 
+    {
+        tableName: 'accounts_lq_details'
+    });
+    return AccountLQDetail
+}
