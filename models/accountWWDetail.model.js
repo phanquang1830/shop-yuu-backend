69e8f513bf5ee1bbb16c321b3aff1d98f2c1e1bc
PhanQuang <phanquang1830@gmail.com>
2025-06-02 22:07:23 +0700

    Feat: add account ww detail model

---
+++ models/accountWWDetail.model.js
@@ -0,0 +1,34 @@
+import { DataTypes } from "sequelize";
+
+export default (sequelize) =>{
+    const AccountWWDetail = sequelize.define('AccountWWDetail', {
+        account_ww_id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            allowNull: false
+        },
+        server: {
+            type: DataTypes.STRING(50)
+        },
+        union_lever: {
+            type: DataTypes.INTEGER
+        },
+        limited_characters: {
+            type: DataTypes.TEXT
+        },
+        limited_weapons: {
+            type: DataTypes.TEXT
+        },
+        premium_currency: {
+            type: DataTypes.STRING(100)
+        },
+        login_method: {
+            type: DataTypes.STRING(100)
+        }
+    },
+    {
+        tableName: 'account_ww_details'
+    });
+
+    return AccountWWDetail
+}
