1030664d9aed21efb2759419252e6a4fa3821dda
PhanQuang <phanquang1830@gmail.com>
2025-06-05 22:25:06 +0700

    Feat: add generateToken & verifyToken

---
+++ utils/jwt.js
@@ -0,0 +1,18 @@
+import jwt from 'jsonwebtoken';
+
+const generateToken = (userId) =>{
+    return jwt.sign({id: userId}, process.env.JWT_SECRET, {expiresIn: '1h'})
+} 
+
+const verifyToken = (token) =>{
+    try {
+        return jwt.verify(token, process.env.JWT_SECRET)
+    } catch (error) {
+        return null;
+    }
+}
+
+export {
+    generateToken,
+    verifyToken
+}
