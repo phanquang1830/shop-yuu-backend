0c2bf254d18b2aef10e9d40708899ce19cd1ab2a
PhanQuang <phanquang1830@gmail.com>
2025-06-05 16:31:36 +0700

    Feat: add Genshin & Lienquan route

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ import dotenv from 'dotenv';
 import { notFound, errorHandler } from "./middlewares/errorHandler.middleware.js";
 import "./models/index.model.js"
 import wutheringwavesRoute from './routes/wutheringwaves.route.js'
+import genshinRoute from './routes/genshin.route.js'
+import lienquanRoute from './routes/lienquan.route.js'
 
 dotenv.config();
 
@@ -13,6 +15,8 @@ const app = express();
 app.use(express.json());
 
 app.use('/api/wutheringwaves', wutheringwavesRoute)
+app.use('/api/genshin', genshinRoute)
+app.use('/api/lienquan', lienquanRoute)
 
 app.use(notFound)
 app.use(errorHandler)
