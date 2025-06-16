5c83308fdaf1d9eb1da614855c554d0c37c152ca
PhanQuang <phanquang1830@gmail.com>
2025-06-16 10:37:33 +0700

    Feat: Add auth middleware

---
+++ middlewares/auth.middleware.js
@@ -0,0 +1,25 @@
+import asyncHandler from 'express-async-handler';
+import { UnauthorizedError } from '../utils/error/index.js';
+import {verifyToken} from '../utils/jwt.js' 
+
+const protect = asyncHandler(async (req, res, next) =>{
+    const authHeader = req.headers.authorization;
+
+    if(!authHeader || !authHeader.startsWith('Bearer')) {
+        throw new UnauthorizedError("Chưa có token, vui lòng đăng nhập!")
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    const decode = verifyToken(token);
+
+    if(!decode) {
+        throw new UnauthorizedError("Token không hợp lệ hoặc hết hạn!")
+    }
+
+    req.user = decode;
+
+    next();
+})
+
+export default protect
