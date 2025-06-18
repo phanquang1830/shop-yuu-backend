d9b2344369ae1e98bb00eb1b325ec8363dc48c4c
PhanQuang <phanquang1830@gmail.com>
2025-06-18 09:39:25 +0700

    Feat: Add handleMulterError

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,16 +1,20 @@
-import express from 'express';
-import {
-    getProfile,
-    updateProfile
-}  from '../controllers/user.controller.js'
-import protect from '../middlewares/auth.middleware.js';
-import upload from '../middlewares/upload.middleware.js';
+import express from "express";
+import { getProfile, updateProfile } from "../controllers/user.controller.js";
+import protect from "../middlewares/auth.middleware.js";
+import upload from "../middlewares/upload.middleware.js";
+import handleMulterError from "../middlewares/multerError.middleware.js";
 
 const router = express.Router();
 
-router.get('/profile', protect, getProfile)
+router.get("/profile", protect, getProfile);
 
 //upload.single('avatar'): dòng này cho phép upload duy nhất 1 file và tên field là avatar
-router.post('/update-profile', protect,upload.single('avatar'),updateProfile)
+router.post(
+  "/update-profile",
+  protect,
+  upload.single("avatar"),
+  handleMulterError,
+  updateProfile
+);
 
-export default router
+export default router;
