0a63333edc41e6fbff424127316264cc2a281ed9
PhanQuang <phanquang1830@gmail.com>
2025-06-18 09:38:26 +0700

    Refactor upload middleware to use Cloudinary instead of local storage

--- middlewares/upload.middleware.js
+++ middlewares/upload.middleware.js
@@ -1,49 +1,32 @@
-import multer from "multer";          // Multer dùng để xử lý multipart/form-data (upload file)
-import path from "path";              // Path giúp xử lý đường dẫn và phần mở rộng file
+import multer from "multer";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import cloudinary from "../config/cloudinary.js";
 
-// -----------------------------
-// CẤU HÌNH NƠI LƯU FILE
-// -----------------------------
-const storage = multer.diskStorage({
-  // Định nghĩa thư mục lưu file upload
-  destination: (req, file, cb) => {
-    // null ở đây là không có lỗi
-    //cb: callback – dùng để báo cho multer biết đường dẫn sẽ lưu file
-    cb(null, "uploads/");            // Lưu vào thư mục 'uploads' ngay gốc project
-  },
-
-  // Định nghĩa tên file sau khi lưu
-  filename: (req, file, cb) => {
-    const ext = path.extname(file.originalname);   // Lấy phần mở rộng, ví dụ: .jpg, .png
-    const uniqueName = `avatar-${Date.now()}${ext}`; // Tạo tên file mới duy nhất, tránh trùng
-    cb(null, uniqueName);                         // Trả về tên file mới
-  },
-});
-
-// -----------------------------
-// BỘ LỌC FILE (CHỈ CHO ẢNH)
-// -----------------------------
-// Khi người dùng upload 1 file lên, mỗi file đều có một thông tin gọi là mimetype, ví dụ:
-
-// | File ảnh JPEG | image/jpeg |
-// | File ảnh PNG | image/png |
-// | File PDF | application/pdf |
-// | File Word | application/vnd.openxmlformats-officedocument.wordprocessingml.document |
-
+// bộ lọc chỉ cho upload ảnh:
 const fileFilter = (req, file, cb) => {
   if (file.mimetype.startsWith("image/")) {
-    // Nếu file là ảnh thì cho phép upload
     cb(null, true);
   } else {
-    // Nếu không phải ảnh, từ chối và báo lỗi
     cb(new Error("Chỉ chấp nhận file ảnh!"), false);
   }
 };
 
-// -----------------------------
-// KHỞI TẠO MIDDLEWARE MULTER
-// -----------------------------
-const upload = multer({ storage, fileFilter }); 
-// Khai báo cấu hình sử dụng multer gồm nơi lưu và lọc file
+// Dùng storage Cloudinary, không lưu ở local nữa
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: "avatar", //Tên folder trên Cloudinary,
+    allowed_formats: ["jpg", "jpeg", "png", "webp"],
+    transformation: [{ width: 300, height: 300, crop: "limit" }],
+  },
+});
+
+const upload = multer({
+  storage,
+  fileFilter,
+  limits: {
+    fileSize: 2 * 1024 * 1024, // 2MB = 2 * 1024 * 1024 bytes
+  },
+});
 
 export default upload;
