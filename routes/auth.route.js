31d135f16eeb9ab85b1b578cd032da64a15368cc
PhanQuang <phanquang1830@gmail.com>
2025-06-18 09:35:44 +0700

    Feat: Add forgotPassword & resetPassword route

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,10 +1,18 @@
 import express from "express";
-import { loginUser ,registerUser, verifyOtp } from "../controllers/auth.controller.js";
+import { 
+    loginUser ,
+    registerUser, 
+    verifyOtp, 
+    forgotPassword, 
+    resetPassword 
+} from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/verify-otp", verifyOtp);
-router.post("/login", loginUser)
+router.post("/login", loginUser);
+router.post("/forgot-password", forgotPassword);
+router.post("/reset-password", resetPassword);
 
 export default router;
