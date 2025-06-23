8989946e38ca4ae3b0b4a0f16bcaced60836c778
PhanQuang <phanquang1830@gmail.com>
2025-06-23 15:14:31 +0700

    Feat: Add paymentRoute & orderRoute

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 import express from "express";
-import dotenv from 'dotenv';
-import cors from 'cors';
+import dotenv from "dotenv";
+import cors from "cors";
 
-import { notFound, errorHandler } from "./middlewares/errorHandler.middleware.js";
-import "./models/index.model.js"
-import wutheringwavesRoute from './routes/wutheringwaves.route.js'
-import genshinRoute from './routes/genshin.route.js'
-import lienquanRoute from './routes/lienquan.route.js'
-import authRoute from './routes/auth.route.js'
-import userRoute from './routes/user.route.js'
+import {
+  notFound,
+  errorHandler,
+} from "./middlewares/errorHandler.middleware.js";
+import "./models/index.model.js";
+import wutheringwavesRoute from "./routes/wutheringwaves.route.js";
+import genshinRoute from "./routes/genshin.route.js";
+import lienquanRoute from "./routes/lienquan.route.js";
+import authRoute from "./routes/auth.route.js";
+import userRoute from "./routes/user.route.js";
 import contactRoute from "./routes/contact.route.js";
 import cartRoute from "./routes/cart.route.js";
+import paymentRoute from "./routes/payment.route.js";
+import orderRoute from "./routes/order.route.js";
 
 dotenv.config();
 
@@ -18,23 +23,27 @@ const PORT = parseInt(process.env.PORT) || 3000;
 const app = express();
 
 app.use(express.json());
-app.use(cors({
-  origin: process.env.CLIENT_URL, // hoặc dùng "*" nếu chưa cần bảo mật
-  credentials: true, // nếu anh gửi cookie hoặc Authorization
-}));
-app.use('/uploads', express.static('uploads'));
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL, // hoặc dùng "*" nếu chưa cần bảo mật
+    credentials: true, // nếu anh gửi cookie hoặc Authorization
+  })
+);
+app.use("/uploads", express.static("uploads"));
 
-app.use('/api/wutheringwaves', wutheringwavesRoute)
-app.use('/api/genshinimpact', genshinRoute)
-app.use('/api/lienquan', lienquanRoute)
-app.use('/api/auth', authRoute)
-app.use('/api/users', userRoute)
-app.use('/api/contact', contactRoute)
-app.use('/api/cart', cartRoute);
+app.use("/api/wutheringwaves", wutheringwavesRoute);
+app.use("/api/genshinimpact", genshinRoute);
+app.use("/api/lienquan", lienquanRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/contact", contactRoute);
+app.use("/api/cart", cartRoute);
+app.use("/api/payment", paymentRoute);
+app.use("/api/order", orderRoute);
 
-app.use(notFound)
-app.use(errorHandler)
+app.use(notFound);
+app.use(errorHandler);
 
-app.listen(PORT, () =>{
-    console.log(`App listen on PORT ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`App listen on PORT ${PORT}`);
+});
