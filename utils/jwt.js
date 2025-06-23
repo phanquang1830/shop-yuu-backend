1a00d22e08d689385feff5a84a92e83be72569bb
PhanQuang <phanquang1830@gmail.com>
2025-06-23 10:43:56 +0700

    ExpiresIn token to 30d

--- utils/jwt.js
+++ utils/jwt.js
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken';
 
 const generateToken = (userId) =>{
-    return jwt.sign({id: userId}, process.env.JWT_SECRET, {expiresIn: '1h'})
+    return jwt.sign({id: userId}, process.env.JWT_SECRET, {expiresIn: '30d'})
 } 
 
 const verifyToken = (token) =>{
